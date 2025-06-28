22546f2cf028dab2a13c4355c41ab9a3895da046
AbigailGV <abigailguadarrama13@gmail.com>
2025-06-28 15:06:01 -0600

    customizing style and creating Boundary class

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const canvas = document.querySelector("canvas");
-// game in 2d
+// context game in 2d
 const c = canvas.getContext("2d");
+
+// customize width and height
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Boundaries {
+  // pass an object so we don't have to memorize order or variables
+  constructor({ position }) {
+    this.position = position;
+    // we want baundaries to be squares
+    this.width = 40;
+    this.height = 40;
+  }
+}
